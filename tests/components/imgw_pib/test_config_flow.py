e341eefac249b9729c90922a4686cf752ff8a963
Maciej Bieniek <478555+bieniu@users.noreply.github.com>
2024-04-25 22:07:48 +0200

    First test

--- tests/components/imgw_pib/test_config_flow.py
+++ tests/components/imgw_pib/test_config_flow.py
@@ -1,94 +1,63 @@
 """Test the IMGW-PIB config flow."""
 
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock, Mock, PropertyMock, patch
+
+from imgw_pib.exceptions import ApiError
 
 from homeassistant import config_entries
-from homeassistant.components.imgw_pib.config_flow import CannotConnect, InvalidAuth
-from homeassistant.components.imgw_pib.const import DOMAIN
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.components.imgw_pib.const import CONF_STATION_ID, DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
+
+HYDROLOGICAL_STATIONS = {"123": "Station Name"}
+HYDROLOGICAL_DATA = Mock(
+    station="Station Name",
+    river="River Name",
+    station_id="123",
+)
 
 
 async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
     """Test we get the form."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
+    with (
+        patch("homeassistant.components.imgw_pib.ImgwPib.update_hydrological_stations"),
+        patch(
+            "homeassistant.components.imgw_pib.ImgwPib.hydrological_stations",
+            new_callable=PropertyMock,
+            return_value=HYDROLOGICAL_STATIONS,
+        ),
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_USER}
+        )
     assert result["type"] == FlowResultType.FORM
     assert result["errors"] == {}
 
-    with patch(
-        "homeassistant.components.imgw_pib.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
+    with (
+        patch("homeassistant.components.imgw_pib.ImgwPib.update_hydrological_stations"),
+        patch("homeassistant.components.imgw_pib.ImgwPib._update_hydrological_details"),
+        patch(
+            "homeassistant.components.imgw_pib.ImgwPib.get_hydrological_data",
+            return_value=HYDROLOGICAL_DATA,
+        ),
+        patch(
+            "homeassistant.components.imgw_pib.ImgwPib.hydrological_stations",
+            new_callable=PropertyMock,
+            return_value=HYDROLOGICAL_STATIONS,
+        ),
     ):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                CONF_HOST: "1.1.1.1",
-                CONF_USERNAME: "test-username",
-                CONF_PASSWORD: "test-password",
+                CONF_STATION_ID: "123",
             },
         )
         await hass.async_block_till_done()
 
     assert result["type"] == FlowResultType.CREATE_ENTRY
-    assert result["title"] == "Name of the device"
+    assert result["title"] == "River Name (Station Name)"
     assert result["data"] == {
-        CONF_HOST: "1.1.1.1",
-        CONF_USERNAME: "test-username",
-        CONF_PASSWORD: "test-password",
-    }
-    assert len(mock_setup_entry.mock_calls) == 1
-
-
-async def test_form_invalid_auth(
-    hass: HomeAssistant, mock_setup_entry: AsyncMock
-) -> None:
-    """Test we handle invalid auth."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.imgw_pib.config_flow.PlaceholderHub.authenticate",
-        side_effect=InvalidAuth,
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                CONF_HOST: "1.1.1.1",
-                CONF_USERNAME: "test-username",
-                CONF_PASSWORD: "test-password",
-            },
-        )
-
-    assert result["type"] == FlowResultType.FORM
-    assert result["errors"] == {"base": "invalid_auth"}
-
-    # Make sure the config flow tests finish with either an
-    # FlowResultType.CREATE_ENTRY or FlowResultType.ABORT so
-    # we can show the config flow is able to recover from an error.
-    with patch(
-        "homeassistant.components.imgw_pib.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                CONF_HOST: "1.1.1.1",
-                CONF_USERNAME: "test-username",
-                CONF_PASSWORD: "test-password",
-            },
-        )
-        await hass.async_block_till_done()
-
-    assert result["type"] == FlowResultType.CREATE_ENTRY
-    assert result["title"] == "Name of the device"
-    assert result["data"] == {
-        CONF_HOST: "1.1.1.1",
-        CONF_USERNAME: "test-username",
-        CONF_PASSWORD: "test-password",
+        CONF_STATION_ID: "123",
     }
     assert len(mock_setup_entry.mock_calls) == 1
 
@@ -97,49 +66,30 @@ async def test_form_cannot_connect(
     hass: HomeAssistant, mock_setup_entry: AsyncMock
 ) -> None:
     """Test we handle cannot connect error."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
+    with (
+        patch("homeassistant.components.imgw_pib.ImgwPib.update_hydrological_stations"),
+        patch(
+            "homeassistant.components.imgw_pib.ImgwPib.hydrological_stations",
+            new_callable=PropertyMock,
+            return_value=HYDROLOGICAL_STATIONS,
+        ),
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_USER}
+        )
 
     with patch(
-        "homeassistant.components.imgw_pib.config_flow.PlaceholderHub.authenticate",
-        side_effect=CannotConnect,
+        "homeassistant.components.imgw_pib.ImgwPib.update_hydrological_stations",
+        side_effect=ApiError("API Error"),
     ):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                CONF_HOST: "1.1.1.1",
-                CONF_USERNAME: "test-username",
-                CONF_PASSWORD: "test-password",
+                CONF_STATION_ID: "123",
             },
         )
 
     assert result["type"] == FlowResultType.FORM
-    assert result["errors"] == {"base": "cannot_connect"}
+    assert result["errors"] == {"base": "api_error"}
 
-    # Make sure the config flow tests finish with either an
-    # FlowResultType.CREATE_ENTRY or FlowResultType.ABORT so
-    # we can show the config flow is able to recover from an error.
-
-    with patch(
-        "homeassistant.components.imgw_pib.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                CONF_HOST: "1.1.1.1",
-                CONF_USERNAME: "test-username",
-                CONF_PASSWORD: "test-password",
-            },
-        )
-        await hass.async_block_till_done()
-
-    assert result["type"] == FlowResultType.CREATE_ENTRY
-    assert result["title"] == "Name of the device"
-    assert result["data"] == {
-        CONF_HOST: "1.1.1.1",
-        CONF_USERNAME: "test-username",
-        CONF_PASSWORD: "test-password",
-    }
     assert len(mock_setup_entry.mock_calls) == 1
