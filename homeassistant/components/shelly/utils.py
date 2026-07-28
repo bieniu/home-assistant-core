0e03c4d6c5dba1e4793f73001916cbd77818ce23
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-07-28 23:07:03 +0200

    Allow BLU TRV entities for non-BLU Gateway devices

--- homeassistant/components/shelly/utils.py
+++ homeassistant/components/shelly/utils.py
@@ -13,7 +13,6 @@ from aioshelly.const import (
     DEFAULT_COAP_PORT,
     DEFAULT_HTTP_PORT,
     MODEL_1L,
-    MODEL_BLU_GATEWAY_G3,
     MODEL_DIMMER,
     MODEL_DIMMER_2,
     MODEL_EM3,
@@ -896,9 +895,6 @@ def remove_stale_blu_trv_devices(
     hass: HomeAssistant, rpc_device: RpcDevice, entry: ConfigEntry
 ) -> None:
     """Remove stale BLU TRV devices."""
-    if rpc_device.model != MODEL_BLU_GATEWAY_G3:
-        return
-
     dev_reg = dr.async_get(hass)
     devices = dev_reg.devices.get_devices_for_config_entry_id(entry.entry_id)
     config = rpc_device.config
