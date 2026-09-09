5dac2cf37b64df1551103a2b6c01131ae318110e
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-09-09 12:30:56 +0200

    Remove ircode from VIRTUAL_COMPONENTS

--- homeassistant/components/shelly/const.py
+++ homeassistant/components/shelly/const.py
@@ -290,7 +290,7 @@ DEVICES_WITHOUT_FIRMWARE_CHANGELOG = (
 
 CONF_GEN = "gen"
 
-VIRTUAL_COMPONENTS = ("boolean", "button", "enum", "ircode", "number", "text")
+VIRTUAL_COMPONENTS = ("boolean", "button", "enum", "number", "text")
 VIRTUAL_COMPONENTS_MAP = {
     "binary_sensor": {"types": ["boolean"], "modes": ["label"]},
     "button": {"types": ["button", "ircode"], "modes": ["button"]},
